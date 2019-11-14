53034b423630073b7cd69f329cefc246055ba559
Gabriel Soria <gabriel_fs@hotmail.com>
2019-11-14 02:43:08 -0300

    se agrega el contenido de la clase 33

---
+++ Clase 33/home-banking/js/homebanking.js
@@ -0,0 +1,47 @@
+//Declaración de variables
+
+//Ejecución de las funciones que actualizan los valores de las variables en el HTML.
+window.onload = function() {
+    cargarNombreEnPantalla();
+    actualizarSaldoEnPantalla();
+    actualizarLimiteEnPantalla();
+}
+
+
+//Funciones que tenes que completar
+function cambiarLimiteDeExtraccion() {
+
+}
+
+function extraerDinero() {
+
+}
+
+function depositarDinero() {
+
+}
+
+function pagarServicio() {
+
+}
+
+function transferirDinero() {
+
+}
+
+function iniciarSesion() {
+
+}
+
+//Funciones que actualizan el valor de las variables en el HTML
+function cargarNombreEnPantalla() {
+    document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
+}
+
+function actualizarSaldoEnPantalla() {
+    document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
+}
+
+function actualizarLimiteEnPantalla() {
+    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
+}
