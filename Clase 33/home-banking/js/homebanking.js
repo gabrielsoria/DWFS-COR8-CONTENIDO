3847170a47ab427539daf12c49c78843075ec3ad
Nico Alvarez <49794196+NiAlvarez@users.noreply.github.com>
2019-11-14 21:45:07 -0300

    Ejercicio objeto first commit

--- Clase 33/home-banking/js/homebanking.js
+++ Clase 33/home-banking/js/homebanking.js
@@ -1,4 +1,62 @@
 //Declaración de variables
+class Usuario {
+    constructor(nombre,pin,cuenta) {
+        this.nombre = nombre;
+        this.pin = pin;
+        this.cuenta = cuenta;
+    }
+        validarPin(){
+            return pin == this.pin;
+        }
+}
+
+class cuenta {
+    constructor(nroCuenta, limiteExtraccion, cuentasAsociadas){
+        this.saldo = 0;
+        this.saldoAnterior = 0;
+        this.nroCuenta = nroCuenta;
+        this.limiteExtraccion = limiteExtraccion;
+        this.cuentasAsociadas = cuentasAsociadas;
+    }
+    extraerDinero(monto){
+        return this.saldo - monto;
+    }
+    validarSaldoExtraccion(monto){
+        if (this.saldo >= monto){
+            return true;
+        }
+    }
+    validarLimites(monto){
+        if (monto <= this.limiteExtraccion){
+            return true
+        }
+    }
+    depositarDinero(monto){
+        return this.saldo + monto;
+    }
+    validarCuentaAmiga(nroCuenta){
+        let result = this.cuentasAsociadas.find(x => x == nroCuenta);
+        }
+    transferirDinero(monto,cuenta){
+        return  this.saldo - monto;
+        }
+    
+    pagarServicio(servicio){
+        return this.saldo - servicio.precio;
+    }
+    actualizarLimite(){
+        
+    }    
+}
+
+    
+
+
+
+
+
+
+
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
