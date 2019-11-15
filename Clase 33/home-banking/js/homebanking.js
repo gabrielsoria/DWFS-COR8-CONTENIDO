858f41a7407f094b3da3b5d54aaec30a077b018c
Pau Gigena <mariapaulagigena@gmail.com>
2019-11-14 21:43:27 -0300

    primer comiteo

--- Clase 33/home-banking/js/homebanking.js
+++ Clase 33/home-banking/js/homebanking.js
@@ -1,5 +1,61 @@
 //Declaración de variables
 
+var Usuario=function(nombre, pin, cuenta){
+    this.nombre=nombre;
+    this.pin=pin;
+    this.cuenta=cuenta;
+    this.validarPin=function(nro){
+        return nro==this.pin;
+    }
+}
+
+var Cuenta=function(nroCuenta, saldo, limiteDeExtraccion, cuentasAsociadas, saldoAnterior){
+    this.nroCuenta=nroCuenta;
+    this.saldo=saldo;
+    this.limiteDeExtraccion=limiteDeExtraccion;
+    this.cuentasAsociadas=cuentasAsociadas;
+    this.saldoAnterior=saldoAnterior;
+    this.extraerDinero=function(monto){
+        this.saldo-=monto;
+
+    };
+    this.validarSaldoExtraccion=function(monto){
+        return monto>0 && this.saldo>=monto;
+
+    };
+    this.validarLimite=function(monto){
+        return this.limiteDeExtraccion<=monto;
+
+    };
+    this.depositarDinero=function(monto){
+        this.saldo+=monto;
+
+    };
+    this.validarCuentaAmiga=function(nrocuenta){
+        let result=this.cuentasAsociadas.find(function(x){
+            return x==nroCuenta;
+        })
+    }
+    this.transferirDinero=function(monto, cuenta){
+        this.saldo-=monto;
+
+    };
+    this.pagarServicio=function(servicio){
+        this.saldo-=servicio.precio;
+
+    };
+    this.actualizarLimite=function(monto){
+        this.limiteDeExtraccion=monto;
+
+    };
+
+}
+
+var Servicio=function(nombre, precio){
+    this.nombre=nombre;
+    this.precio=precio;
+}
+
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
     cargarNombreEnPantalla();
