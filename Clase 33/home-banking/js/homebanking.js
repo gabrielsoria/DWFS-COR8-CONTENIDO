da0ab54a38eef746fd8531967a95873f7a23f922
diaz-yamil <yamilcorpor2016@gmail.com>
2019-11-14 21:45:07 -0300

    version 2 diaz-indiveri

---
+++ Clase 33/home-banking/js/homebanking.js
@@ -0,0 +1,120 @@
+//Declaración de variables
+
+var pinUsuario = 1234;
+var saldo = 500;
+
+
+
+class Usuario{
+    constructor(usuario, cuenta, pin){
+        this.usuario = usuario;
+        this.cuenta = cuenta;
+        this.pin = pin;
+    }
+
+    get validarPin(pin) {
+        if(this.pin == pin){
+            return this.pin == pin;
+        }
+    }
+}
+
+class Cuenta{
+    constructor(nroCuenta, saldo, limiteExtraccion, cuentasAsociadas, saldoAnterior){
+        this.nroCuenta = nroCuenta;
+        this.saldo = saldo;
+        this.limiteExtraccion = limiteExtraccion;
+        this.cuentasAsociadas = cuentasAsociadas;
+        this.saldoAnterior = saldoAnterior;
+    }
+    get extraerPlata(monto){
+        return saldo - monto;
+    }
+    get validarSaldo(monto){
+        if(this.saldo<monto) {
+            return 
+        }
+    }
+    get validarLimite(monto){
+        if(monto>this.limiteExtraccion){
+            return false;
+        }
+    }
+    get depositarPlata(monto){
+        return this.saldo + monto;
+    }
+
+    get transferirplata(monto){
+        return this.saldo - monto;
+    }
+    get pagarFactura(Servicio){
+        return this.saldo<=this.precio;
+    }
+    get actualizarLimiteSaldo(monto){
+        return monto == this.limiteExtraccion;
+    }
+
+
+    /*array.includes(element)*/
+}
+
+class Servicio{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+}
+
+
+
+
+
+
+
+//Ejecución de las funciones que actualizan los valores de las variables en el HTML.
+window.onload = function() {
+    cargarNombreEnPantalla();
+    actualizarSaldoEnPantalla();
+    actualizarLimiteEnPantalla();
+}
+
+
+//Funciones que tenes que completar
+function cambiarLimiteDeExtraccion() {
+
+}
+
+function extraerDinero() {
+
+}
+
+function depositarDinero() {
+
+}
+
+function pagarServicio() {
+
+}
+
+function transferirDinero() {
+
+}
+
+function iniciarSesion() {
+
+}
+
+//Funciones que actualizan el valor de las variables en el HTML
+function cargarNombreEnPantalla() {
+    document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
+}
+
+function actualizarSaldoEnPantalla() {
+    document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
+}
+
+function actualizarLimiteEnPantalla() {
+    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
+}
+
