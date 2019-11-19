84fefc16001fabf016d2bb689926486afb3f97bc
Gabriel Soria <gabriel_fs@hotmail.com>
2019-11-19 19:30:50 -0300

    se agrega la solucion del homebanking

--- Clase 33/home-banking/js/homebanking.js
+++ Clase 33/home-banking/js/homebanking.js
@@ -1,10 +1,77 @@
 //Declaración de variables
 
+var Usuario=function(nombre, pin, cuenta){
+    this.nombre=nombre;
+    this.pin=pin;
+    this.cuenta=cuenta;
+    this.validarPin=function(nro){
+        return nro==this.pin;
+    }
+}
+
+var Cuenta=function(nroCuenta, saldo, limiteDeExtraccion, cuentasAsociadas, saldoAnterior){
+    this.nroCuenta=nroCuenta;
+    this.saldo=saldo;
+    this.limiteDeExtraccion=limiteDeExtraccion;
+    this.cuentasAsociadas=cuentasAsociadas;
+    this.saldoAnterior=saldoAnterior;
+    this.extraerDinero=function(monto){
+        this.saldo-=monto;
+
+    };
+    this.validarSaldoExtraccion=function(monto){
+        return monto>0 && this.saldo>=monto;
+
+    };
+    this.validarLimite=function(monto){
+        return this.limiteDeExtraccion<=monto;
+
+    };
+    this.depositarDinero=function(monto){
+        this.saldo+=monto;
+
+    };
+    this.validarCuentaAmiga=function(nrocuenta){
+        let result=this.cuentasAsociadas.find(function(x){
+            return x==nroCuenta;
+        })
+    }
+    this.transferirDinero=function(monto, cuenta){
+        this.saldo-=monto;
+
+    };
+    this.pagarServicio=function(servicio){
+        this.saldo-=servicio.precio;
+
+    };
+    this.actualizarLimite=function(monto){
+        this.limiteDeExtraccion=monto;
+
+    };
+
+}
+
+var Servicio=function(nombre, precio){
+    this.nombre=nombre;
+    this.precio=precio;
+}
+
+
+let cuenta = new Cuenta(1111, 0, 0, [2222, 4444], 0 );
+let usuario = new Usuario("Gabriel", "4444", cuenta);
+
+let servicios = { 
+    luz: new Servicio("Servicio de luz", 300),
+    agua: new Servicio("Servicio de agua", 200),
+    gas: new Servicio("Servicio de gas", 400),
+}
+
+
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
-    cargarNombreEnPantalla();
-    actualizarSaldoEnPantalla();
-    actualizarLimiteEnPantalla();
+    cargarNombreEnPantalla(usuario);
+    actualizarSaldoEnPantalla(usuario.cuenta);
+    actualizarLimiteEnPantalla(usuario.cuenta);
 }
 
 
@@ -34,14 +101,14 @@ function iniciarSesion() {
 }
 
 //Funciones que actualizan el valor de las variables en el HTML
-function cargarNombreEnPantalla() {
-    document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
+function cargarNombreEnPantalla(usuario) {
+    document.getElementById("nombre").innerHTML = "Bienvenido/a " + usuario.nombre;
 }
 
-function actualizarSaldoEnPantalla() {
-    document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
+function actualizarSaldoEnPantalla(cuenta) {
+    document.getElementById("saldo-cuenta").innerHTML = "$" + cuenta.saldo;
 }
 
-function actualizarLimiteEnPantalla() {
-    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
+function actualizarLimiteEnPantalla(cuenta) {
+    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + cuenta.limiteDeExtraccion;
 }
