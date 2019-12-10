6a3fc32ac01f1dc917486ff9e394e4100b1eb11d
Gabriel Soria <gabriel_fs@hotmail.com>
2019-12-10 00:40:28 -0300

    se agrega mocha y chai a testing

--- Clase 37/home-banking/js/test/homebankingTest.js
+++ Clase 37/home-banking/js/test/homebankingTest.js
@@ -1,29 +1,85 @@
 
+// chequea las propiedades
+function chequearPropiedadesUsuario() {
 
-function testPropiedadesUsuario() {
     let user = new UsuarioClass("", undefined, "");
+  
+    let properties = Object.keys(user);
+  
+    return properties.length == 5;
+  }
+  
+  // chequea las propiedades
+  function chequearValoresPropiedadesUsuario() {
+  
+    let user = new UsuarioClass("Gabriel", undefined, "1");
+  
+    return user.nombre === "Gabriel" 
+        && user.cuenta === undefined 
+        && user.pin === "1";
+  }
+  
+  // chequea las propiedades
+  function chequearValoresPinUsuario() {
+  
+    let user = new UsuarioClass("Gabriel", undefined, "1");
+  
+    return !user.validarPin("2") && user.validarPin("1") && !user.validarPin("");
+  }
 
-    return user.nombre === "" && user.cuenta === undefined && user.pin === "";
-}
+// console.log("Caso de prueba del obj usuario ", chequearPropiedadesUsuario());
+// console.log("Caso de prueba del obj usuario ", chequearValoresPropiedadesUsuario());
+// console.log("Caso de prueba del obj usuario ", chequearValoresPinUsuario());
 
-function testPropiedadesCorrectas() {
-    let user = new UsuarioClass("", undefined, "");
+localStorage.setItem("AccessKey", {test: "holahola", key: "test"})
 
-    let keys = Object.keys(user);
 
-    return keys.length === 3;
-}
 
-// console.log("Test de propiedades ", testPropiedadesUsuario());
-// console.log("Test de propiedades correctas ", testPropiedadesCorrectas());
+  var expect = chai.expect;
+
+//   expect(foo).to.be.a('string');
+//   expect(foo).to.equal('bar');
+//   expect(foo).to.have.lengthOf(3);
+//   expect(beverages).to.have.property('tea').with.lengthOf(3);
+
 
 describe('Test del objeto usuario', function() {
 
     it('Prueba las propiedades', function() {
-        testPropiedadesUsuario();
+        
+        let user = new UsuarioClass("", undefined, "");
+  
+        let properties = Object.keys(user);
+      
+        //return properties.length == 3;
+        expect(properties).to.have.lengthOf(3);
     });
-    it('Prueba los valores de las p', function() {
-        testPropiedadesCorrectas();
+    
+    it('Prueba los valores de las propiedades', function() {
+        
+        let user = new UsuarioClass("Gabriel", undefined, "1");
+  
+        // return user.nombre === "Gabriel" 
+        //     && user.cuenta === undefined 
+        //     && user.pin === "1";
+
+        expect(user.nombre).to.equal("Gabriel");
+        expect(user.cuenta).to.equal(undefined);
+        expect(user.pin).to.equal("1");
+
+
+
+    });
+
+    it('chequea el metodo de validacion de pin de usuario', function() {
+        
+        
+        let user = new UsuarioClass("Gabriel", undefined, "1");
+  
+        return !user.validarPin("2") 
+        && user.validarPin("1") 
+        && !user.validarPin("");
+
     });
 
 
