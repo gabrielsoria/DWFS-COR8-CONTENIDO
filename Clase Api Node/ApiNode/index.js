b43964178a9a7f2f7c58d71c50a12a804cf9b430
Gabriel Soria <gabriel_fs@hotmail.com>
2020-03-10 19:55:06 -0300

    se agrega contenido

--- Clase Api Node/ApiNode/index.js
+++ Clase Api Node/ApiNode/index.js
@@ -1,22 +1,103 @@
-console.log("hola hola desde node.!!!");
+console.log("hola hola desde node!!");
 
 var express = require("express");
+var parser = require("body-parser");
+var mysql = require('mysql');
+
 var app = express();
 
-app.get('/Sumar', function(request, response) {
+app.use(parser.json());
 
-    console.log("estos son los parametros", 
-        request.query["numero1"], request.query["numero2"] );
 
-    let suma = request.query["numero1"] + request.query["numero2"];
-    response.send("El valor de la suma es " + suma );
+var conne;
 
+
+app.get("/clientes", function(request, response) {
+    
+    let query = 'select * from cliente';
+
+    conne.query(query, function (error, results, fields) {
+        
+        if (error) {            
+            console.log("error!!", error);
+            throw error;
+         } 
+        
+        response.send(JSON.stringify(results));
+    });
+        
 });
 
-app.listen(8080, () =>{
+app.get("/cliente/:id", function(request, response) {
+    
+    let idCliente = request.params.id;
 
-    console.log("server iniciado.!!!");
+    let query = `select * from cliente 
+                where IdCliente = ${idCliente}`;
 
+    conne.query(query, function (error, results, fields) {
+        
+        if (error) {            
+            console.log("error!!", error);
+            throw error;
+         } 
+        
+        response.send(JSON.stringify(results));
+    });
+        
+});
+
+app.post("/cliente", function(request, response) {
+
+    // console.log("Body recibido en el post.", request.body);
+    let query = `INSERT INTO pedidosdb.cliente
+    (Nombre,
+    Apellido,
+    Dni)
+    VALUES
+    (
+        '${ request.body.Nombre }',
+        '${ request.body.Apellido }',
+        '${ request.body.Dni }');`
+
+
+    conne.query(query, function (error, results, fields) {
+    
+        if (error) {            
+            console.log("error!!", error);
+            throw error;
+        } 
+        
+        response.send("El cliente ha sido agregado con exito.");
+    });
+
+    
+});
+
+app.put("/sumar", function(request, response) {
+
+    console.log("metodo sumar invocado desde put.");
+    response.send("response desde la api desde put!!");
+});
+
+app.delete("/sumar", function(request, response) {
+
+    console.log("metodo sumar invocado.desde delete");
+    response.send("response desde la api desde delete.!!");
 });
 
 
+
+app.listen(8089, function(){
+    console.log("app iniciada en 8089!!");
+
+    conne = mysql.createConnection({
+        host     : "localhost",
+        port: "3306",
+        user     : "root",
+        password : "mysql",
+        database : "pedidotest"
+    });
+    
+    conne.connect();
+})
