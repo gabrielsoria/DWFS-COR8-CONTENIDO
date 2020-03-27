f6fec04d30b7a232e93540c3c2352d8230dfd01e
Gabriel Soria <gabriel_fs@hotmail.com>
2020-03-26 21:52:33 -0300

    actualizaciones sobre la api

--- Clase Api Node/ApiNode/index.js
+++ Clase Api Node/ApiNode/index.js
@@ -11,6 +11,54 @@ app.use(parser.json());
 app.use(cors());
 
 
+// peliculas?pagina=1&genero=9&anio=2002&cantidad=52&columna_orden=titulo&tipo_orden=ASC
+
+app.get("/peliculas", function mostrarPelicula(request, response){
+    
+
+    let anio = request.query.anio;
+    let titulo = request.query.titulo;
+    let genero = request.query.genero;
+    let orden = request.query.columna_orden;
+    let tipo_orden = request.query.tipo_orden;
+    let pagina = request.query.pagina;
+    let cantidad = (request.query.cantidad = 52) ? request.query.cantidad : undefined
+
+    function crearQuery (anio, titulo, genero, orden, tipo_orden, pagina, cantidad){
+
+        let q = "select * from pelicula where";
+
+        q += anio? ""
+
+        query = "select * from pelicula where (anio = '" + anio + "' or titulo like '%" + titulo + "' or genero_id = '" + genero + "')"
+
+        let query = "select * from pelicula Order by "+ orden + " " + tipo_orden + " LIMIT " + (pagina - 1) * cantidad + "," + cantidad + "";
+
+        if (anio != undefined || titulo != undefined || genero != undefined){
+            query = "select * from pelicula where (anio = '" + anio + "' or titulo like '%" + titulo + "' or genero_id = '" + genero + "')"
+        }
+
+        return query
+    }
+    
+    let query2 = crearQuery(anio, titulo, genero, orden, tipo_orden, pagina, cantidad);
+    conexion.query(query2, function(error, result, fields){
+        if (error) {
+            console.log("error!!", error);
+            throw error;
+        }
+
+        var respuesta = {
+            peliculas: result
+        };
+
+        response.send(JSON.stringify(respuesta));
+    })
+
+});
+
+
+
 
 
 app.get("/clientes", clienteController.getAllClientes);
